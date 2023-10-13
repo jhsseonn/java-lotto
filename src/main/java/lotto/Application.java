6102574a9b741e3972ae93dfe4cf2c08d63d40e9
jhsseonn <hs125bt@gmail.com>
2023-10-13 16:46:44 +0900

    feat: 구입금액 입력받아 로또 구매매수 계산

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해 주세요.");
+
+        int purchase = Integer.parseInt(Console.readLine());
+        int lottoAmount = 0;
+
+        if (purchase%1000==0) {
+            lottoAmount=purchase/1000;
+        }
+
+        System.out.println(lottoAmount+"개를 구매했습니다.");
+
     }
 }
